eae841ceae3667090ef0f78cd18cb893d06f72ec
king lich <iljapuskov@gmail.com>
2024-04-04 13:23:58 +0200

    feat add options with validator

---
+++ Shared/Models/Options.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace Shared.Models;
+
+public class Options
+{
+    [Option("file-log", Required = true, HelpText = "Path to the log file.")]
+    public string FileLog { get; set; }
+    
+    [Option("file-output", Required = true, HelpText = "Path to the output file.")]
+    public string FileOutput { get; set; }
+    
+    [Option("address-start", HelpText = "Start address for IP range.")]
+    public string AddressStart { get; set; }
+    
+    [Option("address-mask", HelpText = "Subnet mask for IP range.")]
+    public int? AddressMask { get; set; }
+}
