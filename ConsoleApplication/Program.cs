65c8106db7373ccff4197a891fce24878db0262a
king lich <iljapuskov@gmail.com>
2024-04-04 13:25:42 +0200

    add program.cs

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+using FluentValidation.Results;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Extensions;
+using Shared.Feature;
+using Shared.Models;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection.AddShared();
+serviceCollection.AddSingleton(args);
+
+
+
