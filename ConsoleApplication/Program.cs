917fee2a054ada2a79e533d5205a9ea6ba4aa706
king lich <iljapuskov@gmail.com>
2024-04-04 13:44:11 +0200

    implement validation of options collection in main

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CommandLine;
 using FluentValidation.Results;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.CommandFactory;
 using Shared.Extensions;
 using Shared.Feature;
 using Shared.Models;
@@ -10,5 +11,24 @@ var serviceCollection = new ServiceCollection();
 serviceCollection.AddShared();
 serviceCollection.AddSingleton(args);
 
+var provider = serviceCollection.BuildServiceProvider();
 
+var commandFactory = provider.GetService<Func<string[], AbstractCommand>>();
+if (commandFactory is null)
+{
+    Console.WriteLine("Command for executing not found :(");
+    return;
+}
+
+var command = commandFactory(args);
+var options = command.ExecuteCommand();
+
+if (options is null)
+{
+    return;
+}
+
+
+
+Console.ReadKey();
 
