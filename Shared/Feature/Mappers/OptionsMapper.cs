21cbb9b6177d7f6721672f6e1925efc477cef0c0
king lich <iljapuskov@gmail.com>
2024-04-06 15:21:58 +0200

    feat mapper

---
+++ Shared/Feature/Mappers/OptionsMapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using Mapster;
+using Shared.Models;
+
+namespace Shared.Feature.Mappers;
+
+public class OptionsMapper : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.ForType<Options, AnalyzerOptions>()
+            .Map(destination => destination.DateStart,
+                source => DateTime.ParseExact(source.DateStart, "dd.MM.yyyy", null,
+                    DateTimeStyles.None))
+            .Map(destination => destination.DateEnd,
+                source => DateTime.ParseExact(source.DateEnd, "dd.MM.yyyy", null,
+                    DateTimeStyles.None));
+    }
+}
