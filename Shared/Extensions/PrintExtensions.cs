9384a8fa9612b87f0969f6ba1248acd2404a506f
king lich <iljapuskov@gmail.com>
2024-04-06 15:23:54 +0200

    feat print extensions

---
+++ Shared/Extensions/PrintExtensions.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+using FluentValidation.Results;
+
+namespace Shared.Extensions;
+
+public static class PrintExtensions
+{
+    private const ConsoleColor ErrorColor = ConsoleColor.Red;
+    private const ConsoleColor InfoColor = ConsoleColor.DarkCyan;
+    public static void Print(this List<ValidationFailure> errors)
+    {
+        var foregroundColor = Console.ForegroundColor;
+        Console.ForegroundColor = ErrorColor;
+        errors.ForEach(error => Console.WriteLine(error.ErrorMessage));
+        Console.ForegroundColor = foregroundColor;
+    }
+    public static void PrintError(string message)
+    {
+        var foregroundColor = Console.ForegroundColor;
+        Console.ForegroundColor = ErrorColor;
+        Console.WriteLine(message);
+        Console.ForegroundColor = foregroundColor;
+    }
+    public static void PrintInfo(string message)
+    {
+        var foregroundColor = Console.ForegroundColor;
+        Console.ForegroundColor = InfoColor;
+        Console.WriteLine(message);
+        Console.ForegroundColor = foregroundColor;
+    }
+}
